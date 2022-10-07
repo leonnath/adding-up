b7baf4059c229c0aea702004371a7c6a87ec2e87
nath <nathgarcia2903@gmail.com>
2022-10-07 11:05:46 +0200

    add the validations is_numeric

--- function.php
+++ function.php
@@ -2,7 +2,12 @@
 
     $numero1=$_POST['valor1'];
     $numero2=$_POST['valor2'];
+
+    is_numeric($numero1);
+    is_numeric($numero2);
+
     $suma=$numero1+$numero2;
 
-    echo $suma
+    echo $suma;
 ?>
+
